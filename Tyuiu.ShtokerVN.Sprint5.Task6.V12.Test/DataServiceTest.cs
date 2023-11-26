e7f4c40f173d34792da7c08ba343e3fc8812f945
Vladimir Shtoker <Tyuiu.ShtokerVN@outlook.com>
2023-11-26 20:37:09 +0500

    Task6

---
+++ Tyuiu.ShtokerVN.Sprint5.Task6.V12.Test/DataServiceTest.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.IO;
+using Tyuiu.ShtokerVN.Sprint5.Task6.V12.Lib;
+
+namespace Tyuiu.ShtokerVN.Sprint5.Task6.V12.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidCalc()
+        {
+            DataService ds = new DataService();
+            string path = @"C:\DataSprint5\InPutDataFileTask6V12.txt";
+
+            var res = ds.LoadFromDataFile(path);
+            int wait = 3;
+            Assert.AreEqual(wait, res);
+        }
+        [TestMethod]
+        public void CheckedExistsFile()
+        {
+            string path = @"C:\DataSprint5\InPutDataFileTask6V12.txt";
+
+            FileInfo fileInfo = new FileInfo(path);
+            bool fileExists = fileInfo.Exists;
+            bool wait = true;
+            Assert.AreEqual(wait, fileExists);
+        }
+    }
+}
---
+++ Tyuiu.ShtokerVN.Sprint5.Task6.V12/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using Tyuiu.ShtokerVN.Sprint5.Task6.V12.Lib;
+
+namespace Tyuiu.ShtokerVN.Sprint5.Task6.V12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #5 | Выполнил: Штокер В.Н. | ИИПб-23-1";
+
+            Console.WriteLine("**************************************************************************************************************************************************************");
+            Console.WriteLine("* Спринт #5                                                                                                                                                  *");
+            Console.WriteLine("* Тема: Обработка текстовых файлов                                                                                                                           *");
+            Console.WriteLine("* Задание #6                                                                                                                                                 *");
+            Console.WriteLine("* Вариант #12                                                                                                                                                *");
+            Console.WriteLine("* Выполнил: Штокер Владимир Николаевич  | ИИПб-23-1                                                                                                          *");
+            Console.WriteLine("**************************************************************************************************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                                                                                                   *");
+            Console.WriteLine("* Найти количество удвоенных букв нн в заданной строке.                                                                                                      *");
+            Console.WriteLine("**************************************************************************************************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                                                                                                           *");
+            Console.WriteLine("**************************************************************************************************************************************************************");
+
+            string path = @"C:\DataSprint5\InPutDataFileTask6V12.txt";
+
+            Console.WriteLine("Данные находятся в файле: " + path);
+
+            Console.WriteLine("**************************************************************************************************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                                                                                                                 *");
+            Console.WriteLine("**************************************************************************************************************************************************************");
+
+            double res = ds.LoadFromDataFile(path);
+            Console.WriteLine(res);
+            Console.ReadKey();
+        }
+    }
+}
